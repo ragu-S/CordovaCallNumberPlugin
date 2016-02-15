dc346092d6b2a34828fadccbe26cf45d0eddaa9b
Ragu Sivanandha <ragu.sivanandha@gmail.com>
2016-02-15 12:46:35 -0500

    Update CallNumber.js

--- www/CallNumber.js
+++ www/CallNumber.js
@@ -3,6 +3,6 @@ var exec = require("cordova/exec");
 
 module.exports = {
     callNumber: function(success, failure, number, bypassAppChooser) {
-        cordova.exec(success, failure, "CallNumber", "callNumber", [number, bypassAppChooser]);
+        exec(success, failure, "CallNumber", "callNumber", [number, bypassAppChooser]);
     }
 }
